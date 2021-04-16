546ae530a60d0ce161e0bbcef9dd9efdeddfe56b
aminzer <minzer.alexey@gmail.com>
2021-04-16 18:31:46 +0300

    fix dir size for unix systems

--- src/iterate_dir_children/iterate_dir_children_recursive.js
+++ src/iterate_dir_children/iterate_dir_children_recursive.js
@@ -11,12 +11,15 @@ async function iterateDirChildrenRecursive(absoluteDirPath, relativeDirPath, onE
     const relativePath = relativeDirPath ? path.join(relativeDirPath, name) : name;
 
     const stats = await stat(absolutePath);
+    const isFile = stats.isFile();
+    const size = isFile ? stats.size : 0;
+
     const fsEntry = new FsEntry({
       name,
       absolutePath,
       relativePath,
-      isFile: stats.isFile(),
-      size: stats.size,
+      isFile,
+      size,
     });
 
     let iterateEntry = fsEntry.isDirectory;
