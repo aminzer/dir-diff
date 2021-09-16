fe8e79fbf5321543f4f6b366caa4b94bec4e5b61
aminzer <minzer.alexey@gmail.com>
2021-09-16 17:31:48 +0300

    refactor mirroring utility

--- src/compare_directories/get_mirrored_fs_entry.ts
+++ src/compare_directories/get_mirrored_fs_entry.ts
@@ -6,22 +6,24 @@ export default async function getMirroredFsEntry(
   fsEntry: FsEntry,
   mirrorRootPath: string,
 ): Promise<FsEntry> {
+  const mirroredFsEntryPath = path.join(mirrorRootPath, fsEntry.relativePath);
+  let mirroredFsEntryStats;
+
   try {
-    const mirroredFsEntryPath = path.join(mirrorRootPath, fsEntry.relativePath);
-    const mirroredFsEntryStats = await stat(mirroredFsEntryPath);
-
-    if (fsEntry.isFile !== mirroredFsEntryStats.isFile()) {
-      return null;
-    }
-
-    return new FsEntry({
-      name: fsEntry.name,
-      absolutePath: mirroredFsEntryPath,
-      relativePath: fsEntry.relativePath,
-      isFile: mirroredFsEntryStats.isFile(),
-      size: mirroredFsEntryStats.size,
-    });
+    mirroredFsEntryStats = await stat(mirroredFsEntryPath);
   } catch (err) {
     return null;
   }
+
+  if (fsEntry.isFile !== mirroredFsEntryStats.isFile()) {
+    return null;
+  }
+
+  return new FsEntry({
+    name: fsEntry.name,
+    absolutePath: mirroredFsEntryPath,
+    relativePath: fsEntry.relativePath,
+    isFile: mirroredFsEntryStats.isFile(),
+    size: mirroredFsEntryStats.size,
+  });
 }
