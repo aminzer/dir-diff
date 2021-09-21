d931efa4d4663340d2930a679998c8eb75b37be0
aminzer <minzer.alexey@gmail.com>
2021-09-21 17:23:32 +0300

    add iterate-dir-children to lib api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
+export { default as iterateDirectoryChildren } from './iterate_directory_children';
 export { default as compareDirectories } from './compare_directories';
