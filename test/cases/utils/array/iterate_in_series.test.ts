d6d0f38c7f3be9fb08c07f53f17bcc4e30ef3f16
aminzer <minzer.alexey@gmail.com>
2021-10-16 00:20:09 +0300

    add test cases

--- test/cases/utils/array/iterate_in_series.test.ts
+++ test/cases/utils/array/iterate_in_series.test.ts
@@ -1,19 +1,61 @@
 import { iterateInSeries } from '../../../../dist/utils/array';
 
 describe('iterateInSeries', () => {
-  it('is resolved to "undefined"', async () => {
-    const returnValue = await iterateInSeries([], () => {});
-
-    expect(returnValue).toBe(undefined);
-  });
-
-  it('calls async callback in series for each array element', async () => {
+  describe('when async callback is executed without rejections for each element', () => {
     let buffer = '';
+    let returnValue;
 
-    await iterateInSeries(['a', 'b', 'c'], async (element: string, index: number) => {
-      buffer += `${element}${index}.`;
+    beforeEach(async () => {
+      buffer = '';
+
+      returnValue = iterateInSeries(['a', 'b', 'c'], async (element: string, index: number) => {
+        buffer += `${element}${index}.`;
+      });
     });
 
-    expect(buffer).toBe('a0.b1.c2.');
+    it('is resolved to "undefined"', async () => {
+      await expect(returnValue)
+        .resolves
+        .toBe(undefined);
+    });
+
+    it('calls async callback in series for each array element', async () => {
+      await returnValue;
+
+      expect(buffer).toBe('a0.b1.c2.');
+    });
+  });
+
+  describe('when async callback rejects with error for some element', () => {
+    let buffer = '';
+    let returnValue;
+
+    beforeEach(async () => {
+      buffer = '';
+
+      returnValue = iterateInSeries(['a', 'b', 'c'], async (element: string, index: number) => {
+        if (element === 'b') {
+          throw new Error('Test error');
+        }
+
+        buffer += `${element}${index}.`;
+      });
+    });
+
+    it('is rejected with corresponding error', async () => {
+      await expect(returnValue)
+        .rejects
+        .toThrow('Test error');
+    });
+
+    it("doesn't call callback for elements after rejected one", async () => {
+      try {
+        await returnValue;
+      } catch (err) {
+        // ignored
+      }
+
+      expect(buffer).toBe('a0.');
+    });
   });
 });
