ae287ecfe84616d6aae31c65b4712b1caeb2d612
aminzer <minzer.alexey@gmail.com>
2025-09-28 22:20:54 +0300

    move expected resources in compareDirectories test into file itself

--- src/compareDirectories/__tests__/compareDirectories.test.ts
+++ src/compareDirectories/__tests__/compareDirectories.test.ts
@@ -1,8 +1,221 @@
-import { join, sep } from 'node:path';
-import expectedSourceEntries from '../../../test/resources/common/expected_source_fs_entries';
-import expectedTargetEntries from '../../../test/resources/common/expected_target_fs_entries';
+import { join, resolve, sep } from 'node:path';
 import { FsEntry } from '@aminzer/traverse-directory';
 import compareDirectories from '../compareDirectories';
+
+const prepareFsEntries = (
+  entries: {
+    name: string;
+    relativePath: string;
+    isFile: boolean;
+    size: number;
+  }[],
+  basePath: string,
+) =>
+  entries.map(
+    (options) =>
+      new FsEntry({
+        ...options,
+        absolutePath: join(basePath, options.relativePath),
+      }),
+  );
+
+const expectedSourceEntries = prepareFsEntries(
+  [
+    {
+      relativePath: '.dot_file_added',
+      name: '.dot_file_added',
+      isFile: true,
+      size: 15,
+    },
+    {
+      relativePath: 'file1_added.txt',
+      name: 'file1_added.txt',
+      isFile: true,
+      size: 15,
+    },
+    {
+      relativePath: 'file2_added.txt',
+      name: 'file2_added.txt',
+      isFile: true,
+      size: 15,
+    },
+    {
+      relativePath: 'file3.txt',
+      name: 'file3.txt',
+      isFile: true,
+      size: 9,
+    },
+    {
+      relativePath: 'file4.txt',
+      name: 'file4.txt',
+      isFile: true,
+      size: 9,
+    },
+    {
+      relativePath: 'file5_modified_content.txt',
+      name: 'file5_modified_content.txt',
+      isFile: true,
+      size: 39,
+    },
+    {
+      relativePath: 'subdir1_added',
+      name: 'subdir1_added',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir1_added${sep}file11_added.txt`,
+      name: 'file11_added.txt',
+      isFile: true,
+      size: 16,
+    },
+    {
+      relativePath: 'subdir2',
+      name: 'subdir2',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir2${sep}file21.txt`,
+      name: 'file21.txt',
+      isFile: true,
+      size: 10,
+    },
+    {
+      relativePath: `subdir2${sep}file22_added.txt`,
+      name: 'file22_added.txt',
+      isFile: true,
+      size: 16,
+    },
+    {
+      relativePath: `subdir2${sep}file23_modified_size.txt`,
+      name: 'file23_modified_size.txt',
+      isFile: true,
+      size: 51,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21`,
+      name: 'subdir21',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21${sep}file211.txt`,
+      name: 'file211.txt',
+      isFile: true,
+      size: 11,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21${sep}file212_added.txt`,
+      name: 'file212_added.txt',
+      isFile: true,
+      size: 17,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21${sep}file213_modified_content.txt`,
+      name: 'file213_modified_content.txt',
+      isFile: true,
+      size: 41,
+    },
+    {
+      relativePath: `subdir2${sep}subdir22_added`,
+      name: 'subdir22_added',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir2${sep}subdir22_added${sep}file221_added.txt`,
+      name: 'file221_added.txt',
+      isFile: true,
+      size: 17,
+    },
+  ],
+  resolve(__dirname, '../../../test/resources/common/source'),
+);
+
+const expectedTargetEntries = prepareFsEntries(
+  [
+    {
+      relativePath: 'file3.txt',
+      name: 'file3.txt',
+      isFile: true,
+      size: 9,
+    },
+    {
+      relativePath: 'file4.txt',
+      name: 'file4.txt',
+      isFile: true,
+      size: 9,
+    },
+    {
+      relativePath: 'file5_modified_content.txt',
+      name: 'file5_modified_content.txt',
+      isFile: true,
+      size: 39,
+    },
+    {
+      relativePath: 'file6_removed.txt',
+      name: 'file6_removed.txt',
+      isFile: true,
+      size: 17,
+    },
+    {
+      relativePath: 'subdir2',
+      name: 'subdir2',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir2${sep}file21.txt`,
+      name: 'file21.txt',
+      isFile: true,
+      size: 10,
+    },
+    {
+      relativePath: `subdir2${sep}file23_modified_size.txt`,
+      name: 'file23_modified_size.txt',
+      isFile: true,
+      size: 53,
+    },
+    {
+      relativePath: `subdir2${sep}file24_removed.txt`,
+      name: 'file24_removed.txt',
+      isFile: true,
+      size: 18,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21`,
+      name: 'subdir21',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21${sep}file211.txt`,
+      name: 'file211.txt',
+      isFile: true,
+      size: 11,
+    },
+    {
+      relativePath: `subdir2${sep}subdir21${sep}file213_modified_content.txt`,
+      name: 'file213_modified_content.txt',
+      isFile: true,
+      size: 41,
+    },
+    {
+      relativePath: 'subdir3_removed',
+      name: 'subdir3_removed',
+      isFile: false,
+      size: 0,
+    },
+    {
+      relativePath: `subdir3_removed${sep}file31_removed.txt`,
+      name: 'file31_removed.txt',
+      isFile: true,
+      size: 18,
+    },
+  ],
+  resolve(__dirname, '../../../test/resources/common/target'),
+);
 
 function expectEqualEntries(fsEntries: FsEntry[], expectedFsEntries: FsEntry[]) {
   expect(fsEntries.every((fsEntry) => fsEntry instanceof FsEntry)).toBe(true);
